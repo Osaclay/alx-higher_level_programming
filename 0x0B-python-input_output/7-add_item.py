7d219e832f58608278d7b98ca692a5b58d77e8ad
Osaclay <osaosun@gmail.com>
2023-01-10 08:13:37 -0800

    python input/output, alx project

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-""" Program that Load, add, save all arguments to a Python list,
-and then save them to a file """
+"""script to add all arguments to a list"""
+
 import sys
-import os
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+try:
+    my_list = load_from_json_file('add_item.json')
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
 
-
-if __name__ == "__main__":
-    data = "add_item.json"
-
-    if os.path.isfile(data):
-        my_list = load_from_json_file(data)
-    else:
-        my_list = []
-
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, data)
+except FileNotFoundError:
+    save_to_json_file(sys.argv[1:], 'add_item.json')
