aacebf225cb0c21c120cfec0c1dea12a5628c85f
Osaclay <osaosun@gmail.com>
2023-01-10 04:04:36 -0800

    python input/output, alx project

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,8 +4,5 @@
 
 def number_of_lines(filename=""):
     """ function that returns the number of lines of a text file """
-    count = 0
-    with open(filename) as f:
-        for line in f:
-            count += 1
-    return (count)
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
