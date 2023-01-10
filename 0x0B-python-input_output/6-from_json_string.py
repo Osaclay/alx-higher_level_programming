0b6cf6bbd75dcca6131f569f7182180ed02be413
Osaclay <osaosun@gmail.com>
2023-01-10 04:27:15 -0800

    python input/output, alx project

--- 0x0B-python-input_output/6-from_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This program returns an object from json string """
-import json
-
-
-def from_json_string(my_str):
-    """ this function returns an object (Python data structure)
-    represented by a JSON string: """
-    return (json.loads(my_str))
