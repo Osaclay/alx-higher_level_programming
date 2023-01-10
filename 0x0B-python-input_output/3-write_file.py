5bd8b02052bb82a42d9575c7b60e73d35c683a91
Osaclay <osaosun@gmail.com>
2023-01-10 04:14:09 -0800

    python input/output, alx project

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This program opens, reads and writes a file """
-
-
-def write_file(filename="", text=""):
-    """ this function writes a string to a text file (UTF8) and
-    returns the number of characters written """
-    with open(filename, 'w', encoding='utf-8') as f:
-        return (f.write(text))
