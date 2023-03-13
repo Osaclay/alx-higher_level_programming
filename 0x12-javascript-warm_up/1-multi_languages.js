c6e920271a3489980f4d4180fc55420e8979c6fe
Osaclay <osaosun@gmail.com>
2023-03-13 12:08:30 -0700

    javascript alx project

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Script that prints C is fun,Python is cool and JavaScript is amazing.
+
+const message = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(message);
