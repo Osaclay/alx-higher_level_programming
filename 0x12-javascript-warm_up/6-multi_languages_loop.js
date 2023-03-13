3f9477a9b7795edd9bb715a3381084da7e768b7c
Osaclay <osaosun@gmail.com>
2023-03-13 12:55:45 -0700

    javascript alx project

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script that prints strings using a loop
+
+let n;
+const message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (n = 0; n < 3; n++) {
+  console.log(message[n]);
+}
